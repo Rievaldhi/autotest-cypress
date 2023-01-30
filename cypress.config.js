a220dc35cc76e0c107ac18a282b63ee832759bb6
Rievaldhi <rievaldhi@gmail.com>
2023-01-30 13:07:41 +0700

    new update

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,13 @@ module.exports = {
   },
 };
 
-// {
-//   "urlBooker": "https://restful-booker.herokuapp.com"
-// }
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+});
