c14965e35d7c113351801688aa07a15c6aad4d43
Rievaldhi <rievaldhi@gmail.com>
2023-02-03 22:35:14 +0700

    bdd cucumber

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # autotest-cypress
- 
+
+cypress 10 + cucumber 
+1. https://www.npmjs.com/package/cypress-cucumber-preprocessor#cypress-configuration
+2. https://kailash-pathak.medium.com/how-to-integrate-cucumber-in-cypress-10-8c91033af9b7
+3. https://www.youtube.com/watch?v=FlQ9Carxeds&list=PLYDwWPRvXB8-8LG2hZv25HO6C3w_vezZb&index=50&ab_channel=JoanMedia
--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,21 @@
-module.exports = {
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-};
-
-const { defineConfig } = require('cypress');
-
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+
+    on("file:preprocessor", bundler);
+    await addCucumberPreprocessorPlugin(on, config);
+
+    return config;
     },
-  },
+    specPattern: "cypress/e2e/**/*",
+},
 });
